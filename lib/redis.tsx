8b3af5ea28a99e02f1bb27a2865423f046665627
J-Nokwal <jagritnokwal9@gmail.com>
2025-09-03 18:08:20 +0530

    refactor: Clean up Redis client configuration by commenting out optional parameters

--- lib/redis.tsx
+++ lib/redis.tsx
@@ -3,7 +3,6 @@ import Redis from "ioredis";
 let redis: Redis | null = null;
 
 export function getRedisClient(): Redis {
-
   if (!redis) {
     if (!process.env.REDIS_HOST || !process.env.REDIS_PORT) {
       throw new Error("Missing Redis configuration in environment variables");
@@ -11,8 +10,8 @@ export function getRedisClient(): Redis {
     redis = new Redis({
       host: process.env.REDIS_HOST,
       port: Number(process.env.REDIS_PORT),
-      password: process.env.REDIS_PASSWORD, // optional
-      tls: process.env.REDIS_TLS === "true" ? {} : undefined, // for managed services like Upstash Enterprise or AWS
+      // password: process.env.REDIS_PASSWORD, // optional
+      // tls: process.env.REDIS_TLS === "true" ? {} : undefined, // for managed services like Upstash Enterprise or AWS
     });
   }
   return redis;
